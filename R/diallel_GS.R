8f15d8c279d609a8970263dc48bc0af36bd38dfb
Greg Keele <gkeele@gmail.com>
2018-06-11 15:11:44 -0400

    Made progress bar an option

--- R/diallel_GS.R
+++ R/diallel_GS.R
@@ -218,7 +218,9 @@ diallel.gibbs <- function(phenotype, sex, is.female=TRUE, mother.str, father.str
       if (i <= burn.in) {
         # Progresses burn-in progress bar
         burnin.counter <- burnin.counter + 1
-        setTxtProgressBar(burnin.pb, burnin.counter)
+        if (use.progress.bar) {
+          setTxtProgressBar(burnin.pb, burnin.counter)
+        }
       }
       if (i == burn.in) {
         cat("\n")
